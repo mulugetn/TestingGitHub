18b951f044b3aa47817f5b829b859ce06ba73c20
Mulugeta <mulugetaalemayehu@yahoo.com>
2013-11-07 10:04:22 -0500

    Initial Commit

---
+++ TestingGitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestingGitHub
+//
+//  Created by MAN on 11/7/13.
+//  Copyright (c) 2013 Mulugeta A Nega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestingGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestingGitHub
+//
+//  Created by MAN on 11/7/13.
+//  Copyright (c) 2013 Mulugeta A Nega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
